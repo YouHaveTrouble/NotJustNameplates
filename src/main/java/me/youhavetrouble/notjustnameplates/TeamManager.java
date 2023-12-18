42a3c11349d40d906d828efdd72b224de12f8b4c
youhavetrouble <youhavetrouble@youhavetrouble.me>
2023-12-18 09:17:22 +0100

    update to 1.20.4

--- src/main/java/me/youhavetrouble/notjustnameplates/TeamManager.java
+++ src/main/java/me/youhavetrouble/notjustnameplates/TeamManager.java
@@ -5,7 +5,7 @@ import net.minecraft.world.scores.PlayerTeam;
 import net.minecraft.world.scores.Scoreboard;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.craftbukkit.v1_20_R2.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_20_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
